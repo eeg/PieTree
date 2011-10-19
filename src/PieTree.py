d1664f250a1567c450547f348a1bd536e2bb1080
Emma Goldberg <emma@skink>
2011-10-19 14:24:39 -0600

    Version 0.3.1.  21 Aug 2009.

--- src/PieTree.py
+++ src/PieTree.py
@@ -1,5 +1,24 @@
 #! /usr/bin/env python
 
+#--------------------------------------------------
+# Copyright 2008 Emma Goldberg
+# 
+# This file is part of PieTree.
+# 
+# PieTree is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# PieTree is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with PieTree.  If not, see <http://www.gnu.org/licenses/>.
+#-------------------------------------------------- 
+
 ######################################################
 # Module:  PieTree.py
 # Author:  Emma Goldberg, based on Walter Brisken's PieTree.c
